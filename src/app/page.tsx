3eb932a4d188f798212209f62aae5c2d818af0f9
Nishu1103 <128515332+Nishu1103@users.noreply.github.com>
2024-02-16 23:44:56 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Servercopy from "./servercopy.tsx";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>hello</h1>
+      <Servercopy></Servercopy>
+      </main>
+  );
+}
